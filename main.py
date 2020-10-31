51c6db66f789ae5c2f0f340abee3b8b136b01bb3
Tomasz Duszyk <tduszyk@gmail.com>
2020-10-31 07:35:18 -0400

    commit

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import math
+from itertools import compress
 
-def distances(X1,Y1, X2, Y2):
-    distance = math.sqrt((X2 - X1)**2 + (Y2 - Y1)**2)
-    return distance
+#x_y = []
+
+def distance(x1, y1, x2, y2: int):
+    dist: float = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    return dist
+
+# def process_file(filename):
+#     open_file = open(filename, "r")
+#     for line in open_file:
+
+
+#process_file('/home/popeye/Repositories/GitHub/distances/175_groups.csv')
+
+#create list with pairs of shots
+shots = [[-1,4],[-2,2],[-1,2],[1,2],[3,3]]
+pairs = list(zip(shots, shots[1:] + shots[:1]))
+
+for i in pairs:
+    #print(i[0][0], i[0][1], i[1][0], i[1][1])
+    print(distance(i[0][0], i[0][1], i[1][0], i[1][1]))
+
+
